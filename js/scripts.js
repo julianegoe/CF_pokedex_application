17e9ed137ecba7622465d719cc423a3862195c45
julianegoe <goersch.juliane@dpa.com>
2021-02-01 21:29:01 +0100

    add dewgong objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let bulbasaur = {name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass", "poison"]};
 let charmander = {name: "Charmander", height: 0.6, weight: 8.5, types: ["fire"]};
 let squirtle = {name: "Squirtle", height: 0.5, weight: 9, types: ["water"]};
-let pokemonList = [bulbasaur, charmander, squirtle];
+let dewgong = {name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]};
+let pokemonList = [bulbasaur, charmander, squirtle, dewgong];
