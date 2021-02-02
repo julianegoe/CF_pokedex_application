b41f83ad15fd5973a1898dbb7e8ec41d34191414
julianegoe <goersch.juliane@dpa.com>
2021-02-02 22:13:02 +0100

    made basic grid layout for pokedex and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 let pokemonList = [{name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass", "poison"]},
 {name: "Charmander", height: 0.6, weight: 8.5, types: ["fire"]},
 {name: "Squirtle", height: 0.5, weight: 9, types: ["water"]},
-{name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]}];
+{name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]},
+{name: "Snorlax", height: 2.1, weight: 460, types: ["normal"]}];
 
-/*let bulbasaur = {name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass", "poison"]};
-let charmander = {name: "Charmander", height: 0.6, weight: 8.5, types: ["fire"]};
-let squirtle = {name: "Squirtle", height: 0.5, weight: 9, types: ["water"]};
-let dewgong = {name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]};*/
+for (let i = 0; i< pokemonList.length; i++) {
+    console.log(pokemonList[i]["name"]);
+    if (pokemonList[i]["height"] < 1) {
+      document.write(`<div class="grid__item">${pokemonList[i]["name"]} (height: ${pokemonList[i]["height"]}m) </div>`);
+    }
+    else {
+        document.write(`<div class="grid__item"> ${pokemonList[i]["name"]} (height: ${pokemonList[i]["height"]}m) <span>- That's quite big!</span> </div>`);
+    }
+  };
