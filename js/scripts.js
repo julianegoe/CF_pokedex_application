b909a3cc588aa654e353d5911a5353735eec23fd
julianegoe <goersch.juliane@dpa.com>
2021-02-02 19:03:44 +0100

    make array of objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
-let bulbasaur = {name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass", "poison"]};
+let pokemonList = [{name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass", "poison"]},
+{name: "Charmander", height: 0.6, weight: 8.5, types: ["fire"]},
+{name: "Squirtle", height: 0.5, weight: 9, types: ["water"]},
+{name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]}];
+
+/*let bulbasaur = {name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass", "poison"]};
 let charmander = {name: "Charmander", height: 0.6, weight: 8.5, types: ["fire"]};
 let squirtle = {name: "Squirtle", height: 0.5, weight: 9, types: ["water"]};
-let dewgong = {name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]};
-let pokemonList = [bulbasaur, charmander, squirtle, dewgong];
+let dewgong = {name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]};*/
