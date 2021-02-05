9d1c0666af382f153aa4ea6139ff9377f5ad6bbb
julianegoe <goersch.juliane@dpa.com>
2021-02-05 15:51:06 +0100

    add favicon and made pokemonRepository object

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,36 @@
-let pokemonList = [
-{name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass", "poison"]},
-{name: "Charmander", height: 0.6, weight: 8.5, types: ["fire"]},
-{name: "Squirtle", height: 0.5, weight: 9, types: ["water"]},
-{name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]},
-{name: "Snorlax", height: 2.1, weight: 460, types: ["normal"]}
-];
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: "Bulbasaur", height: 0.7, weight: 6.9, types: ["grass", "poison"]},
+    {name: "Charmander", height: 0.6, weight: 8.5, types: ["fire"]},
+    {name: "Squirtle", height: 0.5, weight: 9, types: ["water"]},
+    {name: "Dewgong", height: 1.7, weight: 120, types: ["ice", "water"]},
+    {name: "Snorlax", height: 2.1, weight: 460, types: ["normal"]}
+    ];
+
+    function add (newPokemon) {
+      pokemonList.push(newPokemon);
+    };
 
 
-function printPokemonList(pokeArray) {
-  pokeArray.forEach(function (pokemon) {
-      if (pokemon.height < 1) {
-        document.write(`<div class="pokemon-grid__item">${pokemon.name}
-        (height: ${pokemon.height}m) </div>`);
+    function getAll() {
+      return pokemonList
+    };
+
+    return {
+      add: add,
+      getAll: getAll,
       }
-      else {
-          document.write(`<div class="pokemon-grid__item"> ${pokemon.name}
-          (height: ${pokemon.height}m) <span>- That's quite big!</span> </div>`);
-      }
-    })
-};
+  })();
 
-printPokemonList(pokemonList);
+let displayPokemon = (function () {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    if (pokemon.height < 1) {
+      document.write(`<div class="pokemon-grid__item">${pokemon.name}
+      (height: ${pokemon.height}m) </div>`);
+    }
+    else {
+        document.write(`<div class="pokemon-grid__item"> ${pokemon.name}
+        (height: ${pokemon.height}m) <span>- That's quite big!</span> </div>`)
+    }
+  })
+})();
